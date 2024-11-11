f247619e94b13a2b0b0126125f974a4790b4aa86
James Husband <jchusband@proton.me>
2024-11-11 19:18:31 +0000

    nx init

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+export default function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.tailwind file.
+   */
+  return (
+    <div>
+      <div className="wrapper">
+        <div className="container">
+          <div id="welcome">
+            <h1>I Am A Full Stack Developer</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Contact/Contact.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Page from "../../components/Page";
-export default function Contact() {
-  return (
-    <Page heading="Contact" subHeading="lorem ipsum dolor el ta met">
-      <div>dfsf</div>
-    </Page>
-  );
-}
