76e71c9ceb132c9aa8ea5eaaf0920ea1b8a784d4
James Husband <jchusband@proton.me>
2025-03-20 03:03:39 +0000

    chore: refactor layout with config

---
+++ src/config/index.ts
@@ -0,0 +1,1 @@
+export { metadata as siteMetadata } from "./meta/meta";
---
+++ src/config/meta/meta.ts
@@ -0,0 +1,6 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "James Charles",
+  description: "Personal Development Portfolio",
+};
